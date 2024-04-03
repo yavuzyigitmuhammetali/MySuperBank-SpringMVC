f264319af449d7c9a5062ced5490fb41dd02d80f
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-04-03 17:39:22 +0300

    repo

--- src/main/java/com/buddybank/mysuperbank/controller/CustomerController.java
+++ src/main/java/com/buddybank/mysuperbank/controller/CustomerController.java
@@ -1,19 +1,27 @@
 package com.buddybank.mysuperbank.controller;
 
-import lombok.AllArgsConstructor;
+import com.buddybank.mysuperbank.model.Customer;
+import com.buddybank.mysuperbank.service.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@AllArgsConstructor
 @RestController
-@RequestMapping("customer")
+@RequestMapping("/api/customers")
 public class CustomerController {
 
-    @GetMapping("/deneme")
-    public String customerList(){
-        return "deneme";
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping("/getall")
+    public List<Customer> getAllCustomers() {
+        return customerService.getAllCustomers();
     }
 }
---
+++ src/main/java/com/buddybank/mysuperbank/service/CustomerService.java
@@ -0,0 +1,23 @@
+package com.buddybank.mysuperbank.service;
+
+import com.buddybank.mysuperbank.model.Customer;
+import com.buddybank.mysuperbank.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CustomerService {
+
+    private final CustomerRepository customerRepository;
+
+    @Autowired
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public List<Customer> getAllCustomers() {
+        return customerRepository.findAll();
+    }
+}

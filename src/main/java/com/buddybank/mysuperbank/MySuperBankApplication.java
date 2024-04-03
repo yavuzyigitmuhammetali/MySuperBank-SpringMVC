11463447409e5924bed539a00ff4aed9f8427124
yavuzyigitmuhammetali <yavuzyigitmuhammetali@gmail.com>
2024-04-03 16:42:30 +0300

    My Super Bank is awesome!

---
+++ src/main/java/com/buddybank/mysuperbank/MySuperBankApplication.java
@@ -0,0 +1,13 @@
+package com.buddybank.mysuperbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MySuperBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MySuperBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/buddybank/mysuperbank/MySuperBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buddybank.mysuperbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MySuperBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
